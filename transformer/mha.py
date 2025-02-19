f2a3ba8706ae34e1312675fadae3ea20172ee92f
OvrK12 <krauso@tcd.ie>
2025-02-19 17:41:25 +0100

    Added MHA

---
+++ transformer/mha.py
@@ -0,0 +1,36 @@
+import torch
+from torch import nn
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self,emb_dim, num_heads):
+        super().__init__()
+        self.emb_dim = emb_dim
+        self.num_heads = num_heads
+        self.head_dim = emb_dim // num_heads
+
+        self.Q = nn.Linear(emb_dim, emb_dim)
+        self.K = nn.Linear(emb_dim, emb_dim)
+        self.V = nn.Linear(emb_dim, emb_dim)
+
+        self.linear = nn.Linear(emb_dim, emb_dim)
+    
+    def forward(self, x, mask):
+        batch_size = x.shape[0]
+
+        # shape after this step: batch_size x num_heads x seq_length x emb_dim
+        q = self.Q(x).view(batch_size, -1, self.num_heads, self.head_dim).transpose(1,2)
+        k = self.K(x).view(batch_size, -1, self.num_heads, self.head_dim).transpose(1,2)
+        v = self.V(x).view(batch_size, -1, self.num_heads, self.head_dim).transpose(1,2)
+
+        # output has shape batch_size x num_heads x seq_length x seq_length
+        out = torch.matmul(q, k.transpose(-2, -1)) / self.head_dim ** 0.5
+        if mask is not None:
+            out = out.masked_fill(mask == 0, float("-inf"))
+
+        out = torch.nn.functional.softmax(out, dim=-1)
+        # output has shape batch_size x num_heads x seq_length x emb_dim
+        out = torch.matmul(out, v)
+         # output has shape batch_size x seq_length x emb_dim
+        out = out.transpose(1,2).contiguous().view(batch_size, -1, self.emb_dim)
+
+        return self.linear(out)
